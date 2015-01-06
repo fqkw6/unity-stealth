ed4d63c32edebf78d751e4d609483edf1f8ed644
Illia Litviak <litvil@gmail.com>
2015-01-06 11:50:29 +0200

    cctv camera script

--- Assets/Scripts/CCTVPlayerDetection.cs
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CCTVPlayerDetection : MonoBehaviour {
+public class CCTVPlayerDetection : MonoBehaviour
+{
+    private GameObject player;
+    private LastPlayerSighting lastPlayerSighting;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.player);
+        lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Vector3 relPlayerPos = player.transform.position - transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, relPlayerPos, out hit))
+            {
+                if (hit.collider.gameObject == player)
+                {
+                    lastPlayerSighting.position = player.transform.position
+                }
+            }
+            Debug.Log("У леса на опушке");
+            
+        }
+    }
 }
